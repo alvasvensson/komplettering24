e8054c4484068d754d60ce3d25140b076262e4f5
Alva Svensson <alva.svensson@elev.ga.ntig.se>
2024-04-15 13:22:58 +0200

    GeneriskaKlasserKomplettering

---
+++ komplettering/Program.cs
@@ -0,0 +1,16 @@
+﻿Stack<string> cardStack = new Stack<string>() {};
+Queue<string> hand = new Queue<string>();
+
+cardStack.Push("spader 2");
+cardStack.Push("hjärter 5");
+cardStack.Push("ruter kung");
+
+Console.ReadLine();
+
+while(true){
+Console.WriteLine($"Kortet du drog som nu läggs till i din hand är {cardStack.Peek()}");
+string cardYouGrab = cardStack.Pop();
+hand.Enqueue(cardYouGrab);
+Console.ReadLine();
+}
+ 
